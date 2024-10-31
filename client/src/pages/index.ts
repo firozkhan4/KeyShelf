0c7a4706361be1454b8100ccd83cd9c62e7c9587
firozkhan4 <firozkhan907979@gmail.com>
2024-10-31 16:15:27 +0530

    first commit

---
+++ client/src/pages/index.ts
@@ -0,0 +1,13 @@
+import Login from "./Login";
+import Home from "./Home";
+import Register from "./Register";
+import NotFound from "./NotFound";
+
+
+
+export{
+    Login,
+    Home,
+    Register,
+    NotFound
+}
---
+++ client/src/routes/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Home, Login, NotFound, Register } from "../pages/index";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
